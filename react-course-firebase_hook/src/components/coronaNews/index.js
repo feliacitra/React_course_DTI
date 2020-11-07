f7ee135308c79e3835674c60f3d686118db80047
feliacitra <feliacitra28@gmail.com>
2020-11-07 07:54:40 +0700

    component news

--- react-course-firebase_hook/src/components/coronaNews/index.js
+++ react-course-firebase_hook/src/components/coronaNews/index.js
@@ -1,6 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import app from '../../services/firebase';
+import Cards from '../cards';
 import 'firebase/database';
+
+const convertTanggal = (tanggal) => {
+  const tanggalLokal = new Date(tanggal).toLocaleString('id-ID', {
+    dateStyle: 'full',
+  });
+  return tanggalLokal;
+};
 
 const CoronaNews = () => {
   const [news, setNews] = useState([]);
@@ -17,11 +25,34 @@ const CoronaNews = () => {
   }, []);
 
   console.log(news);
-
   return (
     <div>
       <h2>data corona</h2>
       {isLoading ? <p>loading</p> : <p>data</p>}
+      {/* <p>{JSON.stringify(news)}</p> */}
+      <div>
+        {news.map((newsItem) => {
+          return (
+            <div>
+              <h1 style={{ marginTop: '2em' }}>
+                {convertTanggal(newsItem.date)}
+              </h1>
+              <div>
+                {newsItem.activity.map((activityItem) => {
+                  return (
+                    <a href={activityItem.url} target="_blank" rel="noreferrer">
+                      <Cards>
+                        <h4>{activityItem.title}</h4>
+                        <p>{activityItem.desc}</p>
+                      </Cards>
+                    </a>
+                  );
+                })}
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
